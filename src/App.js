3de934fcf89b73470d8fbf0e59c4994c258c6e35
yadavDilip7 <dilipyadav4464@gmail.com>
2023-06-15 07:47:59 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+import {ChatEngine} from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () =>{
+    return(
+        <ChatEngine
+        height ='100vh'
+        projectID ="4884f3eb-785a-4134-b6cb-dadd9eefb44d"
+        userName="yaduvanshidilip"
+        userSecret ="12345"
+        renderChatFeed={(chatAppProps)=><ChatFeed {...chatAppProps}/>}
+        />
+    );
 }
-
 export default App;
---
+++ src/components/ChatFeed.jsx
@@ -0,0 +1,33 @@
+import MessageForm from "./MessageForm";
+import MyMessage from "./MyMessage";
+import TheirMessage from "./TheirMessage";
+const ChatFeed = (props) => {
+    const {chats, activeChat, userName, messages }= props;
+    const chat = chats && chats[activeChat];
+    console.log(chat,userName, messages);
+    const renderMessages = () =>{
+        const keys = Object.keys(messages);
+        return keys.map((key,index) =>{
+            const message = messages[key];
+            const lastMessageKey =index ===0?null:keys[index-1];
+            const isMyMessage = userName ===message.sender.username;
+
+        })
+    }
+    return (
+        <div key={`msg_${index}`} style={{width:'100%'}}>
+            <div className="message-block">
+                {
+                    isMyMessage
+                    ? <MyMessage/>
+                    :<TheirMessage/>
+                }
+
+            </div>
+            <div className = "read-recipts">
+
+            </div>
+        </div>
+    );
+}
+export default ChatFeed;
